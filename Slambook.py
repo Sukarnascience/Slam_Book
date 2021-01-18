6668b2447093daa5ea8d4278a4f1213aa76b4d5b
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2021-01-18 21:45:14 +0530

    Update Slambook.py

--- Slambook.py
+++ Slambook.py
@@ -1,5 +1,13 @@
-import mysql.connector as sql
-from tkinter import *
-from tkinter import messagebox
-import time
+import mysql.connector as mysql
 
+account = ""
+
+def printout():
+    dataout=open("YourData.txt",'w')
+    connection = mysql.connect(user='mysql', password='jana1234', host='localhost', database='mysql')
+    checker = connection.cursor()
+    checker.execute("SELECT * FROM {};".format(account))
+    data = checker.fetchall()
+    for i in data:
+        pass
+    # taking break 
