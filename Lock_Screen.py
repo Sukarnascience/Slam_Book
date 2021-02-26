d231749b7a712537b7e70bc95d9b9e03bd8ce4bf
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2021-02-27 01:22:43 +0530

    Update Lock_Screen.py

--- Lock_Screen.py
+++ Lock_Screen.py
@@ -1,82 +1,43 @@
 from tkinter import *
 from tkinter import messagebox
 import Verification as MyOTP
-import mysql.connector as mysql
-import Slambook as myBook
+import mysql.connector as sql
 
-GetOTP=None
-move=None
-screen = Tk()
-screen1 = Tk()
-screen2 = Tk()
+def GatewayPage():
 
-connection = mysql.connect(user='mysql', password='jana1234', host='localhost', database='mysql')
+    UN=StringVar()
+    PS=StringVar()
+    PN=IntVar()
+    def signup():
+        datafrom=sql.connect(host="localhost",passwd="jana1234",user="root",database="school")
 
-def checkLogin(username,password):
-    checker = connection.cursor()
-    checker.execute("SELECT * FROM loginData;")
-    data = checker.fetchall()
-    if([username,password] in data):
-        messagebox.showinfo("Last step","Great Your account is exsisting\nSo, Now enter your Phone No. For OTP")
-    else:
-        messagebox.showwarning("Sorry","Something went wrong\nPlease check your details or you might not have account!")
-
-def signupDataIn(username,password,phoneNoForNew):
-    checker = connection.cursor()
-    checker.execute("INSERT INTO loginData values('{}','{}','{}');".format(username,password,phoneNoForNew))
-    connection.commit()
-    messagebox.showinfo("Great","Your Account has successfully created")
-
-
-def lockscreen(stay):   
-    screen.geometry("200x40")
-    if(stay==True):       
-        screen.configure(background='black')
-        screen.title("Lock Screen")
-        login = Button(screen,text="Login")
-        signup = Button(screen,text="Signup")
-        login.place(x=55, y=8)
-        signup.place(x=105, y=8)
-        screen.mainloop()
-    else:
-        screen.destroy()
-
-def giveMeOTP():
-    global GetOTP
-    phoneNO=myOTPV.get()
-    print("yea sended OTP to no.",str(phoneNO))
-    GetOTP = MyOTP.generateOTP(phoneNO)
-    print(GetOTP)
-
-def userOTP():
-    global move
-    if(str(GetOTP)==str(enterYOTP.get())):
-        screen1.destroy()
-    else:
-        messagebox.showwarning("Wrong OTP","the OTP which you have given is wrong")
-
-def loginPage(stay):
-    global myOTPV,enterYOTP,screen
-    
-    screen1.geometry("400x400")
-    if(stay==True):
-        screen1.title("Login")
-        myOTPV=Entry(screen1)
-        myOTPV.place(x=50,y=300)
-        sendOTP = Button(screen1,text="Send OTP",command = giveMeOTP)
-        sendOTP.place(x=240,y=297)
+        a=UN.get()
+        b=PS.get()
+        c=PN.get()
         
+        cc=datafrom.cursor()
+        cc.execute("INSERT INTO account values('{}','{}','{}');".format(a,b,c))
+        datafrom.commit()
+        datafrom.close()
 
-        enterYOTP=Entry(screen1)
-        enterYOTP.place(x=100,y=350)
+    UUN=StringVar()
+    PSW=StringVar()
+    def login():
+        datafrom=sql.connect(host="localhost",passwd="jana1234",user="root",database="school")
 
-        nextPG = Button(screen1,text="Next",command = userOTP)
-        nextPG.place(x=310,y=350)
+        a=UUN.get()
+        b=PSW.get()
+        
+        cc=datafrom.cursor()
+        cc.execute("select * from account where username='{}' and password='{}';".format(a,b))
+        data = cc.fetchall()
+        if(data):
+            pass
+        else:
+            pass
 
-        screen1.mainloop()      
-    else:
-        screen1.destroy()
+    
 
-loginPage(True)
 def ok():
     return True
+
